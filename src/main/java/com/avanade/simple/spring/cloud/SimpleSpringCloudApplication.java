544f215241f07128f043d5b916a181585d79ea95
Rodrigo Tavares <tavares@netplataforma.com.br>
2021-06-09 07:41:27 -0300

    Initial commit

---
+++ src/main/java/com/avanade/simple/spring/cloud/SimpleSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.avanade.simple.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avanade/simple/spring/cloud/SimpleSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avanade.simple.spring.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
